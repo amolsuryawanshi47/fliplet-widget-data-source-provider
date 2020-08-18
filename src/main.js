2a79ff733f7c864f51c11e9c0751caa081adbaf1
Yaroslav Ovdii <yaroslav.o@upplabs.com>
2020-08-18 11:24:37 +0300

    init repo

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import DataSourceProvider from './DataSourceProvider.vue';
+
+new Vue({
+  el: '#fliplet-widget-data-source-provider',
+  render: (createElement) => {
+    return createElement(DataSourceProvider);
+  }
+});
