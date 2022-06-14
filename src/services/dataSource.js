d354e12f400747738cd3f520544ef8a7f0d8be51
Aneri Bania <aneri.bania@xoriant.com>
2022-06-14 18:34:54 +0530

    XOR-7 [FEAT] Ensure excludeTypes added in DataSources.get call

--- src/services/dataSource.js
+++ src/services/dataSource.js
@@ -4,6 +4,7 @@ export const getDataSources = appId => {
   if (appId) {
     getOptions.appId = appId;
     getOptions.includeInUse = true;
+    getOptions.excludeTypes = 'bookmarks,likes,comments';
   }
 
   return Fliplet.DataSources.get(getOptions);
