0d23b131c4060c19516fe45db46c36ca83899314
Aneri Bania <aneri.bania@xoriant.com>
2022-06-20 16:08:54 +0530

    XOR-7 [FEAT] Excluded menu type

--- src/services/dataSource.js
+++ src/services/dataSource.js
@@ -4,7 +4,7 @@ export const getDataSources = appId => {
   if (appId) {
     getOptions.appId = appId;
     getOptions.includeInUse = true;
-    getOptions.excludeTypes = 'bookmarks,likes,comments';
+    getOptions.excludeTypes = 'bookmarks,likes,comments,menu';
   }
 
   return Fliplet.DataSources.get(getOptions);
